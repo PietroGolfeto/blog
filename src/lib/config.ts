977e8cc493c624a62631026ca546871f85e5b804
PietroGolfeto <pggolfeto@gmail.com>
2023-07-17 14:13:47 -0300

    Add SvelteKit Markdown blog

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from "$app/environment"
+
+export const title = "Pietro"
+export const description = "Pietro Grazzioli Golfeto's personal blog"
+export const url = dev ? "http://localhost:5173/" : "TODOCREATEVERCELURL"
